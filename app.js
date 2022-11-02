4041479f887781317bcd2e12b12d8642c0eb73e0
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-02 14:03:50 +0800

    commit for type conversion

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-// primitive
-// const name = 'John Doe' => string
-// const age = 18 => 'Number'
-// const hasKids = true; => boolean
-// const car = null; => null
-// let test => undefined
-// const sym = Symbol(); => Symbol
+// Types Conversion
 
-// Reference Types
-// const hobbies = [1, 2] => Arrays
-// const obj = {a: 1} => Object
+let val;
+// number => string
+// val = String(5);
+
+// bool to string
+// val = String(true)
+
+// ary to string
+// val = String([1, 2, 3, 4])
+
+// .toString()
+
+// String to Number
+// val = Number('5')
+// bool to number
+// val = Number(true)
+// val = Number(false)
+// NaN is an actual value that stands for not a number
+
+// parseInt => parse it as an integer
+
+console.log(val);
+console.log(typeof val);
