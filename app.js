4e9413a48924587d3c95cf65c3db7edbd5a90b8b
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-03 14:40:42 +0800

    string method

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-// Math.abs => return absolute value
-// Math.floor => always rounds down and returns the largest integer less than or equal to a given number.
-// Math.PI => return PI
-// Math.E => return E
-// Math.round() => return the value of a number rounded to the nearest interger
-// Math.ceil() => 四舍五入返回最大端的数字
-// Math.floor(2.8) => return the floor value
-// Math.sqrt(64) => square root
-// Math.pow(8, 2)
+const firstName = 'William';
+const lastName = 'Johnson';
+
+let val;
+
+// val = firstName + lastName;
+
+// concatenation
+// val = firstName + ' ' + lastName
+
+// val = 'Brad ';
+// val += 'Traversy';
+
+// Escaping
+// val = "That's awesome, I can't wait";
+
+// length
+// val = firstName.length;
+
+// concat
+val = firstName.concat('_', lastName);
+
+// upperCase / lowerCase
+
+// val = firstName.indexOf('a');
+
+val = firstName.charAt(firstName.length - 3);
+
+console.log(val);
